2ec7bb6e2519821b9a934c2870f83fa5ac270c4a
jerknose <travis@travisbennett.com>
2017-10-01 22:29:04 -0700

    Webpack will build.

---
+++ webpack-production.config.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = {
+	context: resolve(__dirname, 'src'),
+	entry: [
+		'./index.jsx',
+		// the entry point of our app
+	],
+	output: {
+		filename: 'bundle.js',
+		// the output bundle
+
+		path: resolve(__dirname, 'dist'),
+
+		publicPath: '/'
+		// necessary for HMR to know where to load the hot update chunks
+	},
+	module: {
+		rules: [
+			
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			},
+			{
+			  test: /\.css$/,
+			  loader: 'style-loader'
+			}, {
+			  test: /\.css$/,
+			  loader: 'css-loader'
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2)$/,
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.(jpe?g|gif|svg)$/i,
+				use: [
+					'url-loader?name=images/[name].[ext]&limit=10000',
+					'img-loader?name=images/[name].[ext]'
+				]
+			},
+			{
+				test: /\.(png)$/i,
+				use: [
+					'base64-image-loader',
+				]
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.css']
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+		  minimize: true,
+		  debug: false
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+		  beautify: false,
+		  mangle: {
+		    screw_ie8: false,
+		    keep_fnames: true
+		  },
+		  compress: {
+		    screw_ie8: false
+		  },
+		  comments: false
+		})
+	]
+}
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	context: resolve(__dirname, 'src'),
+
+	entry: [
+		'react-hot-loader/patch',
+		// activate HMR for React
+
+		'webpack-dev-server/client?http://localhost:8080',
+		// bundle the client for webpack-dev-server
+		// and connect to the provided endpoint
+
+		'webpack/hot/only-dev-server',
+		// bundle the client for hot reloading
+		// only- means to only hot reload for successful updates
+
+		'./index.jsx',
+		// the entry point of our app
+	],
+	output: {
+		filename: 'bundle.js',
+		// the output bundle
+
+		path: resolve(__dirname, 'dist'),
+
+		publicPath: '/'
+		// necessary for HMR to know where to load the hot update chunks
+	},
+
+	devtool: 'cheap-source-map',//'inline-source-map',
+
+	devServer: {
+		hot: true,
+		// enable HMR on the server
+
+		contentBase: resolve(__dirname, 'dist'),
+		// match the output path
+
+		publicPath: '/'
+		// match the output `publicPath`
+	},
+	module: {
+		rules: [
+			
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			},
+			{
+			  test: /\.css$/,
+			  loader: 'style-loader'
+			}, {
+			  test: /\.css$/,
+			  loader: 'css-loader'
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2)$/,
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.(jpe?g|gif|svg)$/i,
+				use: [
+					'url-loader?name=images/[name].[ext]&limit=10000',
+					'img-loader?name=images/[name].[ext]'
+				]
+			},
+			{
+				test: /\.(png)$/i,
+				use: [
+					'base64-image-loader'
+				]
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.css']
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		// enable HMR globally
+
+		new webpack.NamedModulesPlugin(),
+		// prints more readable module names in the browser console on HMR updates
+
+		// new ExtractTextPlugin('styles.css')
+		// export css to separate file
+
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+};
